0720ba77a0de0625a7c0aab82251dba2c6795682
Hamza Jorio <hamza.jorio@sharetec.ma>
2020-03-22 11:09:30 +0100

    Updates

---
+++ precache-manifest.7607711709949e044bb2612e2f59b95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696926a72a183fb606ba74be1dd604d1",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "2916ae1702d8423df5ac",
+    "url": "/MonsterRolodex/static/css/main.8a273b68.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/MonsterRolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MonsterRolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2916ae1702d8423df5ac",
+    "url": "/MonsterRolodex/static/js/main.944526a0.chunk.js"
+  },
+  {
+    "revision": "0f9986f51e979b61c54d",
+    "url": "/MonsterRolodex/static/js/runtime-main.c96dc60a.js"
+  }
+]);
